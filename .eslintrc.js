4d4e00dddbe71c6221ad4d0c0cc0f0bb3c8793ff
Ilya P <iopolyus@mts.ru>
2021-09-02 15:40:32 +0300

    chore: add commit linter

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = {
+    env: {
+        commonjs: true,
+        es2021: true,
+        node: true,
+	    webextensions: true,
+    },
+    extends: 'eslint:recommended',
+    parserOptions: {
+        ecmaVersion: 12,
+    },
+};
