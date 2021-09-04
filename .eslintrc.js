f8546bc91f9b04bce960a034ac0399cde50cd932
Ilya P <iopolyus@mts.ru>
2021-09-04 23:36:57 +0300

    chore: move icons into separate dir

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
         commonjs: true,
         es2021: true,
         node: true,
-	    webextensions: true,
+        webextensions: true,
     },
     extends: 'eslint:recommended',
     parserOptions: {
